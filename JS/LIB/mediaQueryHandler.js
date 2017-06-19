994ff60ad9181e33a6a2ef4fad17df15f1bb9a41
ABenjamins <deraikoben@gmail.com>
2017-06-19 12:20:53 +0200

    Added Some struffz

--- JS/LIB/mediaQueryHandler.js
+++ JS/LIB/mediaQueryHandler.js
@@ -3,14 +3,18 @@
  */
 
 
+
+var windowHeight;
+var windowWidth;
+
 var mq = window.matchMedia( "(min-width: 500px)" );
 
 if (mq.matches) {
     // window width is at least 500px
-    var windowHeight = 700;
-    var windowWidth = 1000;
+    windowHeight = 700;
+    windowWidth = 1000;
 } else {
-    var windowHeight = window.innerWidth * 0.7;
-    var windowWidth = window.innerWidth;
+    windowHeight = window.innerWidth * 0.7;
+    windowWidth = window.innerWidth;
     // window width is less than 500px
 }
